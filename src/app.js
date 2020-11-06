537e24bf4f63cfe90befbd80956a2e2ccef899f4
Sberry2020 <69343800+Sberry2020@users.noreply.github.com>
2020-11-06 14:06:18 -0700

    My API key is not working trying to figure out

--- src/app.js
+++ src/app.js
@@ -1,3 +1,3 @@
-let apiKey = "249e71d0ce4c5f5436d8a9d288695e10";
+let apiKey = "a812dde348aef759917fa847a384093f";
 let urlKey = "https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}";
 console.log(urlKey);
