87f7c71001fbdbe8856a9b725c1238134e9a8d9d
Sberry2020 <69343800+Sberry2020@users.noreply.github.com>
2020-11-08 21:03:25 -0700

    Changed main icon image to different kind of image

--- src/app.js
+++ src/app.js
@@ -1,13 +1,14 @@
 function displayWeather(response){
   document.querySelector("#city").innerHTML = response.data.name;
- document.querySelector("#temperature").innerHTML = Math.round (response.data.main.temp
+ document.querySelector("#temperature").innerHTML = Math.round (celsiusTemperature
   );
   document.querySelector("#humidity").innerHTML = response.data.main.humidity;
   document.querySelector("#wind").innerHTML = Math.round(
     response.data.wind.speed
   );
-  document.querySelector("#description").innerHTML = response.data.weather[0].main;
+  document.querySelector("#description").innerHTML = response.data.weather[0].description;
   document.querySelector("#icon").setAttribute("src", `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+  celsiusTemperature = response.data.main.temp;
 }
 function searchCity(city) {
 let apiKey = "a812dde348aef759917fa847a384093f";
@@ -22,19 +23,35 @@ searchCity(city);
 
 function displayFahrenheitTemp(event) {
   event.preventDefault();
-  let fahrenheitTemp = (25 * 9) / 5 + 32;
-  alert(fahrenheitTemp);
+let fahrenheitTemp = (celsiusTemperature * 9) / 5 + 32;
+  let temperatureElement = document.querySelector("#temperature");
+  celsiusLink.classList.remove("active");
+  fahrenheitLink.classList.add("active");
+  temperatureElement.innerHTML = Math.round(fahrenheitTemp);
+} 
+
+function displayCelsiusTemp(event) {
+  event.preventDefault();
+  celsiusLink.classList.add("active");
+  fahrenheitLink.classList.remove("active");
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(celsiusTemperature);
 }
 
 let dateElement = document.querySelector("#date");
 let currentTime = new Date();
 dateElement.innerHTML = `${currentTime}`;
 
+let celsiusTemperature = null;
+
 let searchButton = document.querySelector("#search-form");
 searchButton.addEventListener("submit", form);
 
 let fahrenheitLink = document.querySelector("#fahrenheit-link");
 fahrenheitLink.addEventListener("click", displayFahrenheitTemp);
+
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", displayCelsiusTemp);
 
 
 
